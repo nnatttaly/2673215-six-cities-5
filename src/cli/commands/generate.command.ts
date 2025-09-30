c0f8ba03689a43619bcf9bbf86bb4fb32b72f39d
nnatttaly <dmitrievan730@gmail.com>
2025-09-30 13:32:36 +0500

    Добавлен запрос к серверу и реализация --generate

---
+++ src/cli/commands/generate.command.ts
@@ -0,0 +1,54 @@
+import { appendFile } from 'node:fs/promises';
+import { Command } from './command.interface.js';
+import { MockServerData } from '../../shared/types/index.js';
+import { TSVOfferGenerator } from '../../shared/libs/offer-generator/index.js';
+
+export class GenerateCommand implements Command {
+  private initialData: MockServerData;
+
+  private async load(url: string) {
+    try {
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error(`Не удалось загрузить данные с ${url}`);
+      }
+
+      this.initialData = await response.json();
+    } catch {
+      throw new Error(`Не удалось загрузить данные из ${url}`);
+    }
+  }
+
+  private async write(filepath: string, offerCount: number) {
+    const tsvOfferGenerator = new TSVOfferGenerator(this.initialData);
+    for (let i = 0; i < offerCount; i++) {
+      await appendFile(
+        filepath,
+        `${tsvOfferGenerator.generate()}\n`,
+        { encoding: 'utf8' }
+      );
+    }
+  }
+
+  public getName(): string {
+    return '--generate';
+  }
+
+  public async execute(...parameters: string[]): Promise<void> {
+    const [count, filepath, url] = parameters;
+    const offerCount = Number.parseInt(count, 10);
+
+    try {
+      await this.load(url);
+      await this.write(filepath, offerCount);
+      console.info(`Файл ${filepath} создан!`);
+    } catch (error: unknown) {
+      console.error('Не удалось сгенерировать данные');
+
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
+  }
+}
