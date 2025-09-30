c318e2ccaaebf81c587a3e7cf0dfd39947755272
nnatttaly <dmitrievan730@gmail.com>
2025-09-30 13:22:49 +0500

    Добавлен тип MockServerData

---
+++ src/shared/types/mock-server-data.type.ts
@@ -0,0 +1,9 @@
+export type MockServerData = {
+  titles: string[];
+  descriptions: string[];
+  offerImages: string[];
+  users: string[];
+  emails: string[];
+  passwords: string[];
+  avatars: string[];
+};
