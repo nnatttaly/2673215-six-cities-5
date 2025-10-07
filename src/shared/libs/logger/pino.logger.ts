21a85625b28448ccce8255e9fe18df7c4e3e7e88
nnatttaly <dmitrievan730@gmail.com>
2025-10-07 12:24:25 +0500

    Добавлен DI контейнер с зависимостями

--- src/shared/libs/logger/pino.logger.ts
+++ src/shared/libs/logger/pino.logger.ts
@@ -1,6 +1,8 @@
 import { Logger as PinoInstance, pino } from 'pino';
 import { Logger } from './logger.interface.js';
+import { injectable } from 'inversify';
 
+@injectable()
 export class PinoLogger implements Logger {
   private readonly logger: PinoInstance;
 
