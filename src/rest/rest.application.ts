2aa595e991ee6571392282985ea0d72b00b23971
nnatttaly <dmitrievan730@gmail.com>
2025-10-07 11:00:33 +0500

    Добавлена система конфигурации

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,11 +1,14 @@
 import { Logger } from '../shared/libs/logger/index.js';
+import { Config, RestSchema } from '../shared/libs/config/index.js';
 
 export class RestApplication {
   constructor(
-    private readonly logger: Logger
+    private readonly logger: Logger,
+    private readonly config: Config<RestSchema>,
   ) { }
 
   public async init() {
     this.logger.info('Инициализация приложения');
+    this.logger.info(`Получено значение из окружения $PORT: ${ this.config.get('PORT') }`);
   }
 }
---
+++ src/shared/libs/config/rest.config.ts
@@ -0,0 +1,28 @@
+import { config } from 'dotenv';
+import { Config } from './config.interface.js';
+import { Logger } from '../logger/index.js';
+import { configRestSchema, RestSchema } from './rest.schema.js';
+
+export class RestConfig implements Config<RestSchema> {
+  private readonly config: RestSchema;
+
+  constructor(
+    private readonly logger: Logger
+  ) {
+    const parsedOutput = config();
+
+    if (parsedOutput.error) {
+      throw new Error('Не удалось прочитать файл .env. Возможно, файл не существует.');
+    }
+
+    configRestSchema.load({});
+    configRestSchema.validate({ allowed: 'strict', output: this.logger.info });
+
+    this.config = configRestSchema.getProperties();
+    this.logger.info('Файл .env найден и успешно обработан!');
+  }
+
+  public get<T extends keyof RestSchema>(key: T): RestSchema[T] {
+    return this.config[key];
+  }
+}
