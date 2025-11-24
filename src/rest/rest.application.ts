277da424019fd91874783f072c331b0cf1f0d2fa
nnatttaly <dmitrievan730@gmail.com>
2025-11-24 20:19:02 +0500

    Запущен Express-сервер

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -2,16 +2,22 @@ import { Logger } from '../shared/libs/logger/index.js';
 import { Config, RestSchema } from '../shared/libs/config/index.js';
 import { inject, injectable } from 'inversify';
 import { Component } from '../shared/types/index.js';
+import express, { Express } from 'express';
 import { DatabaseClient } from '../shared/libs/database-client/index.js';
 import { getMongoURI } from '../shared/helpers/index.js';
 
 @injectable()
 export class RestApplication {
+  private server: Express;
+
   constructor(
     @inject(Component.Logger) private readonly logger: Logger,
     @inject(Component.Config) private readonly config: Config<RestSchema>,
-    @inject(Component.DatabaseClient) private readonly databaseClient: DatabaseClient,
-  ) { }
+    @inject(Component.DatabaseClient)
+    private readonly databaseClient: DatabaseClient
+  ) {
+    this.server = express();
+  }
 
   private async _initDb() {
     const mongoUri = getMongoURI(
@@ -25,12 +31,20 @@ export class RestApplication {
     return this.databaseClient.connect(mongoUri);
   }
 
+  private async _initServer() {
+    const port = this.config.get('PORT');
+    this.server.listen(port);
+  }
+
   public async init() {
     this.logger.info('Инициализация приложения.');
-    this.logger.info(`Получено значение из окружения $PORT: ${ this.config.get('PORT') }.`);
 
     this.logger.info('Инициализация базы данных…');
     await this._initDb();
     this.logger.info('Инициализация базы данных завершена.');
+
+    this.logger.info('Инициализации сервера…');
+    await this._initServer();
+    this.logger.info(`🚀🚀🚀 Сервер запущен на http://localhost:${this.config.get('PORT')}`);
   }
 }
